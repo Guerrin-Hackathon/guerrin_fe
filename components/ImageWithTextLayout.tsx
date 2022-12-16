af33d773fe6feaad51216eee71a0de2da6c64132
Gian <gpecile@itba.edu.ar>
2022-12-16 13:32:47 -0300

    feat: add spinner

--- components/ImageWithTextLayout.tsx
+++ components/ImageWithTextLayout.tsx
@@ -1,51 +1,44 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import immutableXService from "../service/ImmutableXService";
+import {useQuery} from "@tanstack/react-query";
+import {Spinner} from "./shared/Spinner";
 
 interface ImageWithTextLayoutProps {
     nft_id: string;
 }
 
+type asset = {
+    title: string;
+    description: string;
+    image_url: string;
+    creator: string;
+}
+
 const ImageWithTextLayout: React.FC<ImageWithTextLayoutProps> = (props) => {
+    const assetQuery = useQuery(['asset'], async () => {
+        return await immutableXService.getAsset(props.nft_id);
+    }, {
+        enabled: !!props.nft_id,
+    });
 
-    const [title, setTitle] = useState("No title fetched");
-    const [desc, setDesc] = useState("Anim anim ea proident occaecat\n" +
-        "                    non minim et. Minim aliqua esse\n" +
-        "                    eu fugiat do quis qui exercitation\n" +
-        "                    cillum officia adipisicing eiusmod\n" +
-        "                    occaecat eiusmod aliqua. Culpa do\n" +
-        "                    occaecat ex adipisicing aliquip magna\n" +
-        "                    pariatur mollit laboris elit ea ipsum\n" +
-        "                    sit laboris exercitation.");
-    const [image_url, setImageUrl]= useState("/messiCopa.jpg");
-    const [creator, setCreator] = useState('Coscu');
-
-    useEffect(() => {
-            async function fetchImmutableX() {
-                let asset = await immutableXService.getAsset(props.nft_id);
-                if (!asset)
-                    return;
-                setTitle(asset.title);
-                setDesc(asset.description);
-                setImageUrl(asset.image_url);
-                setCreator(asset.creator);
-            }
-             fetchImmutableX();
-        }, [props.nft_id]);
+    if (assetQuery.isLoading) {
+        return <Spinner/>;
+    }
 
     return (
         <div className="flex flex-wrap px-20 w-full">
             <div className="w-1/2">
-                <h1 className="text-5xl font-bold mb-10">{title}</h1>
+                <h1 className="text-5xl font-bold mb-10">{assetQuery.data.title}</h1>
                 {/*// <!--En caso de conflicto comunicarse con rober-->*/}
-                <img src={image_url} alt="reward" className="h-[500px]"/>
+                <img src={assetQuery.data.image_url} alt="reward" className="h-[500px]"/>
             </div>
             <div className="w-1/2 mt-20 items-center justify-center">
                 <h1 className="text-4xl font-bold mb-10">Creator:
                     {' '}
-                    <span className="text-red-600">{creator}</span>
+                    <span className="text-red-600">{assetQuery.data.creator}</span>
 
                 </h1>
-                <p>{desc}</p>
+                <p>{assetQuery.data.description}</p>
             </div>
         </div>
     );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {ThemeProvider} from "next-themes";
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
 
+// Create a client
+const queryClient = new QueryClient()
 
 function MyApp({Component, pageProps}: AppProps) {
-    return <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-    </ThemeProvider>
+    return <>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider attribute="class">
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </QueryClientProvider>
+    </>
 }
 
 export default MyApp
